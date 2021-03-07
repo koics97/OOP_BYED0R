47d36f5f4d2faea549226f5bcf2bae76d644b0cf
koics97 <marcell.koics@gmail.com>
2021-03-07 19:52:23 +0100

    beolvas() függvény olvassa be az adatokat
    v1.0.1

--- HF_4.gyak/src/HF/Szemelyfut.java
+++ HF_4.gyak/src/HF/Szemelyfut.java
@@ -6,17 +6,20 @@ public class Szemelyfut {
 
 	public static void main(String[] args) {
 		Szemely person = new Szemely();
+		beolvas(person);
+		System.out.println(person.getAdatok());
+
+	}
+
+	public static void beolvas(Szemely person) {
+
 		Scanner input = new Scanner(System.in);
-		
 		System.out.println("Kerek egy nevet, suly es magassagot! ");
 		person.setNev(input.nextLine());
 		person.setSuly(input.nextInt());
 		person.setMagassag(input.nextDouble());
 		input.close();
-		System.out.println(person.getAdatok());
 
 	}
-	
-	
 
 }
